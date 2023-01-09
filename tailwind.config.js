cad7b1d351ea4039bab1cce4104893c83679b68f
unknown <textile.zero@gmail.com>
2023-01-10 03:04:31 +0600

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{html,js}"],
+	daisyui: {
+		themes: [
+			{
+				evenex: {
+
+					"primary": "#F94E43",
+
+					"secondary": "#E13833",
+
+					"accent": "#F0F0F0",
+
+					"neutral": "#101010",
+
+					"base-100": "#FFFFFF",
+
+					"info": "#3ABFF8",
+
+					"success": "#5DBF7C",
+
+					"warning": "#FBBD23",
+
+					"error": "#F87272",
+				},
+			},
+		],
+	},
+	theme: {
+		extend: {},
+	},
+	plugins: [require( "daisyui" )],
+}
