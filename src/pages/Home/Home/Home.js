819b1133645d583ae4691a44c67709e7bfe25e50
Md.Johirule <johirulislam574206@gmail.comm>
2023-01-10 22:23:41 +0600

    adding Pricing sections.

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { useRef, useState } from "react";
+import Pricing from "../../../compoents/Pricing_Card/Pricing";
 import Reviews from "../../../compoents/Reviews/Reviews";
 import UpcomingEvents from "../UpcomingEvents/UpcomingEvents";
 
@@ -7,6 +8,8 @@ const Home = () => {
 		<div>
 			<UpcomingEvents></UpcomingEvents>
 			<Reviews></Reviews>
+			{/* ---md Johirul islam push today-- */}
+			<Pricing />
 		</div>
 	);
 };
