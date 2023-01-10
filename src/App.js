420a6521e51b74828647f434db81c38ffc41275c
Sourav Saha <shanto.sourav07@gmail.com>
2023-01-10 13:03:39 +0600

    Fake Data Done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Home from './pages/Home/Home/Home';
 
-function App () {
+const App = () => {
 	return (
-		<div className="App">
-			<button className="btn">Button</button>
-			<button className="btn btn-primary">Button</button>
-			<button className="btn btn-secondary">Button</button>
-			<button className="btn btn-accent">Button</button>
-			<button className="btn btn-ghost">Button</button>
-			<button className="btn btn-link">Button</button>
+		<div>
+			<Home></Home>
 		</div>
 	);
-}
+};
 
 export default App;
