5df17396f91a8bfdf8b680a7ba041a7a8bc69fbd
Akash Akter <AkashAkter>
2023-01-10 22:45:40 +0600

    banner and navbar added

--- src/pages/Home/Home/Home.js
+++ src/pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React, { useRef, useState } from "react";
 import Reviews from "../../../compoents/Reviews/Reviews";
+import Banner from "../Banner/Banner";
 import UpcomingEvents from "../UpcomingEvents/UpcomingEvents";
 
 const Home = () => {
 	return (
 		<div>
+			<Banner></Banner>
 			<UpcomingEvents></UpcomingEvents>
 			<Reviews></Reviews>
 		</div>
